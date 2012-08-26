1abd4f1a33a71a4905c1385af77e059b341bfd7e
Michael Porter <michaelporter.0988@gmail.com>
2012-08-25 22:08:57 -0500

    adds rudimentary no-arg handling

--- bin/vinyl.rb
+++ bin/vinyl.rb
@@ -5,6 +5,7 @@ $: << File.join(File.dirname(__FILE__), "/..")
 require 'lib/album.rb'
 require 'lib/artist_solo.rb'
 require 'lib/artist_group.rb'
+require 'lib/menu.rb'
 require 'lib/migration.rb'
 require 'bin/schema.rb'
 
@@ -12,46 +13,14 @@ db = SQLite3::Database.open("my_new_records.db")  # maybe a global here would be
 Album.set_db(db)
 Artist.set_db(db)
 
-def clear_args(steps)
-  ARGV.shift(steps)
-end
-
 res = {}
 
 case ARGV[0]
 when "new", "New", "Add", "add"
-
-  case ARGV[1]
-  when /album/i, /record/i
-    clear_args(2) # allows for gets by clearing the ARGV tray, as it were
-    res = Album.get_new
-    album = Album.new(res)
-    album.save!
-  when /artist/i, /band/i, 
-    clear_args(2)
-    type = ""
-    until ["solo", "group"].include? type
-      puts "solo or group?"
-      type = gets.chomp
-    end
-
-    case type
-    when /solo/i
-      res = ArtistSolo.get_new    # these can be combined, keeping them separate only for now
-      artist = ArtistSolo.new(res)
-      artist.save!
-    when /group/i
-      res = ArtistGroup.get_new
-      artist = ArtistGroup.new(res)
-      artist.save!
-    end # solo, group
-  end # album, artist, etc
+  new_stuff(ARGV)
 when "change", "update"
-  case ARGV[1]
-  when /artist/i
-    clear_args(2)
-    puts "Name:"
-    artist = gets
-    artist = ArtistSolo.find_by_name(artist)
-  end
+  update_stuff(ARGV)
+else
+  prompt_menu
+  Process.exit
 end # new, remove, rm, etc
